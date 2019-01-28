0de9c9df7710d9c1a2438e6bb4175f6aeb4aac42
Auliya Ihsani <auliyaihsani@gmail.com>
2019-01-28 20:46:42 +0700

    spring client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdatedcustomerComponent } from './customer/updated/updatedcustomer.component';
+import { AddcustomerComponent } from './customer/add/addcustomer.component';
+import { ListcustomerComponent } from './customer/list/listcustomer.component';
+import { FormcustomerComponent } from './customer/formcustomer/formcustomer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdatedcustomerComponent,
+    AddcustomerComponent,
+    ListcustomerComponent,
+    FormcustomerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customer/updated/updatedcustomer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updatedcustomer',
+  templateUrl: './updatedcustomer.component.html',
+  styleUrls: ['./updatedcustomer.component.css']
+})
+export class UpdatedcustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
